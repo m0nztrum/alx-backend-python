385d3c93044ce8e71857d5d2843ca8b7e5e91c22
Dragonixj <delafiado@gmail.com>
2024-05-13 16:46:21 +0000

    task 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,12 +5,21 @@ import unittest
 from typing import Any, Dict, Mapping, Sequence
 from unittest.mock import Mock, patch
 
+from parameterized import parameterized
+
 import utils
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """unittest for access_nested_map function"""
 
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(
         self, nested_map: Mapping, path: Sequence, expected: Any
     ) -> None:
